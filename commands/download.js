36a1b06c424a3d93f053dcec992350c4b3c85ba9
JimAppreciator512 <w9HhsgBjzVhashme@protonmail.com>
2022-12-17 02:13:07 -0500

    pretty printed file

--- commands/download.js
+++ commands/download.js
@@ -1,87 +1,90 @@
-import { SlashCommandBuilder } from "discord.js";
-import request from "request";
-import { JSDOM } from "jsdom";
+import { SlashCommandBuilder } from "discord.js"
+import request from "request"
+import { JSDOM } from "jsdom"
 
-const helpMsg = "Usage: /download link: https:\/\/ifunny.co\/(picture|video|gif)\/...";
+const helpMsg =
+	"Usage: /download link: https://ifunny.co/(picture|video|gif)/..."
 
 async function download(interaction) {
 	// getting the url to search in
 	/** @type String */
-	const url = interaction.options.getString("link");
+	const url = interaction.options.getString("link")
 
 	// logging
-	console.log(`Looking in ${url}...`);
+	console.log(`Looking in ${url}...`)
 
 	/// url validation
 
 	// testing if the url is from the expected domain
-	const correctURL = /https:\/\/ifunny\.co\/(picture|video|gif)/;
-	let datatype;
+	const correctURL = /https:\/\/ifunny\.co\/(picture|video|gif)/
+	let datatype
 	if (!url.match(correctURL)) {
 		// the link is invalid
-		console.log(`${url} is an invalid link.`);
-		return interaction.reply({ content: helpMsg, ephemeral: true });
+		console.log(`${url} is an invalid link.`)
+		return interaction.reply({ content: helpMsg, ephemeral: true })
 	} else {
-		datatype = url.match(correctURL)[1];
+		datatype = url.match(correctURL)[1]
 	}
-	
+
 	// evaluating
 	if (datatype === null) {
 		// the link is invalid
-		const msg = `${url} is an invalid iFunny.co link.`;
-		console.log(msg);
-		return interaction.reply(msg);
+		const msg = `${url} is an invalid iFunny.co link.`
+		console.log(msg)
+		return interaction.reply(msg)
 	}
 
 	// trying to get an HTTP request from that url
 	request(url, { json: true }, (err, res, _) => {
 		if (err) {
-			console.log("An error occurred", err);
-			return interaction.reply("Something went wrong when making the HTTP request.")
+			console.log("An error occurred", err)
+			return interaction.reply(
+				"Something went wrong when making the HTTP request."
+			)
 		}
 		if (res) {
 			// transforming the paylod into a DOM
-			const dom = new JSDOM(res.body);
+			const dom = new JSDOM(res.body)
 
 			// making selectors
 			const selEnum = {
-				"gif": "head > link[as=\"image\"]",
-				"picture": "div._3ZEF > img",
-				"video": "div._3ZEF > div > video"
+				gif: 'head > link[as="image"]',
+				picture: "div._3ZEF > img",
+				video: "div._3ZEF > div > video",
 			}
 			const attribEnum = {
-				"gif": "href",
-				"picture": "src",
-				"video": "data-src"
+				gif: "href",
+				picture: "src",
+				video: "data-src",
 			}
 
 			// getting the selector and attribute
-			const sel = selEnum[datatype];
+			const sel = selEnum[datatype]
 			const attrib = attribEnum[datatype]
-			
+
 			// searching the DOM for a datatype tag
-			const el = dom.window.document.querySelector(sel);
+			const el = dom.window.document.querySelector(sel)
 
 			// validation
 			if (el === null) {
-				const msg = `Couldn't find the ${datatype} at ${url}.`;
-				console.log(msg);
-				return interaction.reply({ content: msg, ephemeral: true });
+				const msg = `Couldn't find the ${datatype} at ${url}.`
+				console.log(msg)
+				return interaction.reply({ content: msg, ephemeral: true })
 			}
 
 			// logging
-			console.log(`Found a ${datatype} at ${url}`);
+			console.log(`Found a ${datatype} at ${url}`)
 
 			// need to grab a conditional attribute based on the content type
-			const result = el.getAttribute(attrib);
+			const result = el.getAttribute(attrib)
 
 			// logging
-			console.log(`Returning with the ${datatype} from ${result}`);
-				
+			console.log(`Returning with the ${datatype} from ${result}`)
+
 			// replying to the user with the url
-			return interaction.reply(result);
+			return interaction.reply(result)
 		}
-	});
+	})
 }
 
 const Download = {
@@ -91,11 +94,10 @@ const Download = {
 		.addStringOption(option => {
 			return option
 				.setName("link")
-				.setDescription("An iFunny.co link e.g., ifunny.co\/video\/...")
-				.setRequired(true);
+				.setDescription("An iFunny.co link e.g., ifunny.co/video/...")
+				.setRequired(true)
 		}),
-	execute: download
-};
+	execute: download,
+}
 
-export default Download;
-
+export default Download
