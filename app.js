fba1373864234a7c267270cb4bb95755e5bf7b2c
JimAppreciator512 <w9HhsgBjzVhashme@protonmail.com>
2022-12-17 14:39:01 -0500

    pretty printed files

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ import Download from "./commands/download.js";
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // the list of all bot commands
-client.commands = [
-	Ping,
-	Download
-];
+client.commands = [Ping, Download];
 
 // logging on success
 client.once(Events.ClientReady, c => {
@@ -38,19 +35,24 @@ client.on(Events.InteractionCreate, async interaction => {
 	// trying to execute the command
 	try {
 		// logging
-		console.log(`The command ${interaction.commandName} was executed by ${interaction.member.user.username}.`);
+		console.log(
+			`The command ${interaction.commandName} was executed by ${interaction.member.user.username}.`
+		);
 
 		// executing the command
 		await command.execute(interaction);
 	} catch (error) {
 		// something went wrong executing this command
 		console.error(error);
-		await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+		await interaction.reply({
+			content: "There was an error while executing this command!",
+			ephemeral: true,
+		});
 	}
 });
 
 // establishing a rest connection to discord
-const rest = new REST({ version: '10' }).setToken(config.token);
+const rest = new REST({ version: "10" }).setToken(config.token);
 
 // deploying the commands to discord
 (async () => {
@@ -64,19 +66,28 @@ const rest = new REST({ version: '10' }).setToken(config.token);
 		console.log(payload);
 
 		// logging what commands are sent to the discord API
-		console.log("Loading commands:", payload.map(command => {
-			return command.name;
-		}).join(", "));
+		console.log(
+			"Loading commands:",
+			payload
+				.map(command => {
+					return command.name;
+				})
+				.join(", ")
+		);
 
-		console.log(`Started refreshing ${payload.length} application (/) commands.`);
+		console.log(
+			`Started refreshing ${payload.length} application (/) commands.`
+		);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationCommands(config.clientId),
-			{ body: payload },
+			{ body: payload }
 		);
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		console.log(
+			`Successfully reloaded ${data.length} application (/) commands.`
+		);
 
 		// logging into discord
 		client.login(config.token);
@@ -85,4 +96,3 @@ const rest = new REST({ version: '10' }).setToken(config.token);
 		console.error(error);
 	}
 })();
-
